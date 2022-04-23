4b017871610bbf9be2f4fc549a991396ca2d6b88
Faruk Shaban <faruk.saban@live.com>
2022-04-23 20:40:15 +0200

    Intro to Conditional Statements

---
+++ IntroToConditionalStatements/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+    String ans = "";    
+    if (n % 2 == 1) {
+            ans = "Weird";
+        } else {
+            if ( n >= 6 && n <= 20 ) {
+                ans = "Weird";
+            }else {
+                if(n >= 2 && n <= 5){
+                    ans = "Not Weird";
+            }
+             else{
+                ans = "Not Weird";
+            }   
+            }
+        }
+        System.out.println(ans);
+    }
+}
